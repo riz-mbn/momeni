a3122763f53f1d941d5b8ed0a8edd5041c7d11ac
riz-mbn <rizlee@mybizniche.com>
2021-08-18 07:35:10 +0800

    thank you page

---
+++ templates/thank-you.php
@@ -0,0 +1,24 @@
+<?php 
+    /* Template Name: Thank You template */
+    get_header();
+
+?>
+<section class="page-content">
+	<div class="grid-container">		
+		<div class="grid-x grid-margin-x">
+			<div class="cell large-12">
+				<div class="text-wrap text-center">
+					<h1>Thank You!</h1>
+					<p>Thank you for contacting us, we will get back to you as soon as we can.</p>
+					<p>&nbsp;</p>
+
+					<div class="button-wrap">
+						<a href="<?php echo home_url()?>" class="button primary">Back to homepage</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
+</section>
+
+<?php get_footer() ?>
