19b06708be9abdebc4e3fb8c424de2a16739ea6e
riz-mbn <rizlee@mybizniche.com>
2021-08-19 20:32:51 +0800

    services

---
+++ templates/services.php
@@ -0,0 +1,41 @@
+<?php 
+    /* Template Name: Services template */
+    get_header();
+
+?>
+<section class="services">
+	<figure class="bg"><img src="<?php echo MBN_ASSETS_URI ?>/img/services_bg.png" alt="" width="1065" height="766"></figure>
+	<div class="grid-container">		
+		<div class="grid-x grid-margin-x cols2-s4">
+            <div class="cell xlarge-6 large-6 medium-12 align-self-middle col-copy ">
+				<p class="small">Services</p>
+                <h2><span class="highlight">Momeni Construction</span> offers the full range of construction services,</h2>
+                <p>including design-build, new building construction, site improvement, tenant improvement, post-construction repair, and concrete repair. We are also experienced in virtually any type of construction method and building material, including steel, wood frame, CMU, tilt up, modular, etc.</p>
+            </div>	
+            <div class="cell xlarge-6 large-6 medium-12">
+                <figure class="col-image"><img src="<?php echo MBN_ASSETS_URI ?>/img/services-1.png" alt="" width="507" height="385"></figure>
+				<div class="title_group">
+					<h1 class="hollow_text maskPlay">YOUR VISION</h1>
+					<h1 class="subtitle">BUILT TO LAST.</h1>
+				</div>
+            </div>		
+        </div>
+	</div>	
+</section>
+
+<section class="contact_info">   
+    <div class="grid-container">     
+        <div class="text-group">
+            <a href="tel:702248800">
+                <span><figure><img src="<?php echo MBN_ASSETS_URI ?>/img/icon/icn-phone.svg" alt="" width="24" height="24" /></figure></span>
+                <span>(702) 248-8004</span>
+            </a>
+            <a href="mailto:info@momeniconstruction.com">
+                <span><figure><img src="<?php echo MBN_ASSETS_URI ?>/img/icon/icn-envelope.svg" alt="" width="24" height="24"/></figure></span>
+                <span>info@momeniconstruction.com</span>
+            </a>
+        </div>
+    </div>
+</section>
+
+<?php get_footer() ?>
